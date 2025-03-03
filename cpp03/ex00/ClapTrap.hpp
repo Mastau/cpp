ef850e53e230a13fbe6205a9486213e8d60820f0
Thomas Arnaud <thomarna@student.42angouleme.fr>
2025-03-03 16:10:27 +0100

    DGraou

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+class ClapTrap {
+	public:
+		ClapTrap(const std::string name);
+		ClapTrap(const ClapTrap &copy);
+		~ClapTrap();
+		ClapTrap &operator=(const ClapTrap &copy);
+		void attack(const std::string &target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+	private:
+		std::string _name;
+		unsigned int _hitPts;
+		unsigned int _energyPts;
+		unsigned int _atkDmg;
+};
---
+++ cpp03/ex00/main.cpp
@@ -0,0 +1,16 @@
+#include "ClapTrap.hpp"
+
+int main(void) {
+	ClapTrap claptrap("graou");
+	ClapTrap claptrap2(claptrap);
+
+	claptrap.attack("claptrap2");
+	claptrap2.takeDamage(4);
+	claptrap2.beRepaired(4);
+
+	claptrap2.attack("graou");
+	claptrap.takeDamage(3);
+	claptrap.beRepaired(5);
+
+	//Check if !HitPts, amount == AttackDamage ???
+}
