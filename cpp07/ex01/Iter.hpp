a68820be4499f50076e7c0ecfd2c7b14129c1fe2
Thomas Arnaud <thomarna@student.42angouleme.fr>
2025-04-23 16:02:17 +0200

    Debut cpp07

---
+++ cpp07/ex01/Iter.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstring>
+
+template <typename T, typename Func>
+void iter(T* array, size_t length, Func f) {
+	for (size_t i = 0; i < length; i++) {
+		f(array[i]);
+	}
+}
